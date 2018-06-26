78cd79af2786d406021e3a0f0eb65d8086449cdd
yedhink <yedhin1998@gmail.com>
2018-06-26 22:14:47 +0530

    June 26th 2018 : Create Fuzzy Finding(but slow) and commented async task for fuzzy find

--- AnagramFinder/app/src/main/java/com/example/android/anagramfinder/AnagramsActivity.java
+++ AnagramFinder/app/src/main/java/com/example/android/anagramfinder/AnagramsActivity.java
@@ -34,11 +34,6 @@ public class AnagramsActivity extends AppCompatActivity {
 	private EditText typeView;
 	private Button buttonFind;
 	private AssetManager assetManager;
-
-	/*
-	initially set textview to be null so as to not display anything
-	txv_display is the id given for that particular text view in the XML
-	 */
 	private TextView displayView;
 
 	@Override
@@ -70,8 +65,14 @@ public class AnagramsActivity extends AppCompatActivity {
               the params here are passed to the doInBackground() method
              */
 			new LongOperation().execute(inputStreamKey, inputStreamValue);
+
+			/*
+			add a new listener which would check the user input change and fuzzy finds
+			the corresponding anagrams of the user input if exists.
+			 */
 			typeView.addTextChangedListener(new CustomWatcher());
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			Toast.makeText(this, R.string.no_IO, Toast.LENGTH_SHORT).show();
 		}
 
@@ -83,21 +84,29 @@ public class AnagramsActivity extends AppCompatActivity {
 
 		@Override
 		public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+			/*
+			txv_display is the id given for that particular text view in the XML
+			 */
 			displayView = (TextView) findViewById(R.id.txv_display);
-			changed = s.subSequence(start, start + count).toString();
+			changed = s.subSequence(start, start + count).toString();// to be used in future development
 		}
 
 		@Override
 		public void onTextChanged(CharSequence s, int start, int before, int count) {
+			/*
+			on each update clear the previous results and set the flag whether an anagram is found to be false
+			 */
 			displayView.setText(null);
+			foundAtLeastOneWord = false;
+			// this the newly inputted word
 			newStr = s.toString();
-			edit = s.subSequence(start, start + count).toString();
+			edit = s.subSequence(start, start + count).toString();// to be used in future development
 		}
 
 		@Override
 		public void afterTextChanged(Editable s) {
 			/*
-			the important part where we get the alphabetically sorted form the user input
+			the important part where we get the alphabetically sorted form of the user input,
 			so as to compare it with the values inside the keys file
 			 */
 			sortedInput = getSorted(newStr);
@@ -109,7 +118,10 @@ public class AnagramsActivity extends AppCompatActivity {
 				ArrayList<String> valueList = entry.getValue();
 				// iterate through the ArrayList
 				for (String val : valueList) {
-					// we dont want to redisplay the usernewStr as an anagram
+					/*
+					check if the sorted user input exists as a key. ie:- whether the word has anagrams
+					also make sure that the user input is not redisplayed in output
+					 */
 					if (sortedInput.equals(key) && !val.equalsIgnoreCase(newStr)) {
 						displayView.append(val + "\n");
 						foundAtLeastOneWord = true;
@@ -118,23 +130,107 @@ public class AnagramsActivity extends AppCompatActivity {
 			}
 
 			/*
-			case where no input is provided or no anagram is present for the word , but button was pressed
-			if (newStr.matches("")) {
-				Toast.makeText(AnagramsActivity.this, R.string.no_text, Toast.LENGTH_SHORT).show();
-			} else if (!foundAtLeastOneWord)
+			case where no input is provided or no anagram is present for the word
+			*/
+			if (!foundAtLeastOneWord)
 				Toast.makeText(AnagramsActivity.this, R.string.no_anagram, Toast.LENGTH_SHORT).show();
-
-			/*
-			to hide the keyboard when the output is displayed
-			InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-			try {
-				inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-			} catch (NullPointerException e) {
-				Toast.makeText(AnagramsActivity.this, R.string.null_excep, Toast.LENGTH_SHORT).show();
-			}
-			 */
 		}
 	}
+
+
+/*
+we fuzzy find in background
+	private class fuzzyFind extends AsyncTask<InputStream, Integer, String> {
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+		}
+
+		// populate the hash map in background
+		@Override
+		protected String doInBackground(InputStream... params) {
+			map = new HashMap<>();
+			try {
+                */
+/*
+                create buffered character stream for reading from the key and values files as characters
+                params are inputstreams of those files passed as parameters
+                 *//*
+
+				BufferedReader keyDictionary = new BufferedReader(new InputStreamReader(params[0]));
+				BufferedReader wordsDictionary = new BufferedReader(new InputStreamReader(params[1]));
+				StringTokenizer t;
+
+				// calls onProgressUpdate() method and restricts the user from inputting till map has been loaded
+				publishProgress(1);
+
+				// start reading from both the files simultaneously since the respective key,value pairs will be on the same line
+				while (((key = keyDictionary.readLine()) != null) && (t = new StringTokenizer(wordsDictionary.readLine())) != null) {
+					anagramWords = new ArrayList<>();
+					word = "";
+                    */
+/*
+                    each value read from the values file may contain more than one word. i.e:- a key may have more than
+                    one anagram. so we read the values as tokens(basically splitting into different words) and store
+                    to an array.
+                     *//*
+
+					while (t.hasMoreTokens()) {
+						word = t.nextToken();
+						anagramWords.add(word);
+					}
+                    */
+/*
+                    we store the key and values to a map where values are inside an ArrayList
+                    eg:- iceman : {cinema,anemic}
+                     *//*
+
+					map.put(key, anagramWords);
+				}
+			} catch (IOException e) {
+				System.out.println("IOError");
+			}
+			// give user control when the map is filled and ready
+			return "Type Here";
+		}
+
+		*/
+/*
+		publishProgress() would call this method which would display whats happening in background
+		 *//*
+
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+			typeView.setHintTextColor(getResources().getColor(R.color.colorPrimaryDark));
+			typeView.setHint("Loading...Please Wait");
+            */
+/*
+            restrict user from typing inside the TextField
+             *//*
+
+			typeView.setFocusable(false);
+			buttonFind.setClickable(false);
+		}
+
+		*/
+/*
+		after everything is loaded give user ability to type again
+		also set a hint inside the TextField inorder make users know where to type
+		 *//*
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			typeView.setHint(result);
+			typeView.setFocusableInTouchMode(true);
+			buttonFind.setClickable(true);
+			//CustomWatcher listenerObj = new CustomWatcher();
+			//typeView.addTextChangedListener(listenerObj);
+		}
+	}
+*/
+
 
 	/*
 	Collator class is used to build searching and sorting routines for natural language text.
