4517e47083f76747f847b89b634c028ea79f8df8
yedhink <yedhin1998@gmail.com>
2018-07-13 10:52:30 +0530

    Fix typos

--- AnagramFinder/app/src/main/java/com/example/android/anagramfinder/AnagramsActivity.java
+++ AnagramFinder/app/src/main/java/com/example/android/anagramfinder/AnagramsActivity.java
@@ -29,45 +29,29 @@ public class AnagramsActivity extends AppCompatActivity {
 	private ArrayList<String> anagramWords;
 	private String key, word;
 	private EditText typeView;
-	//private Button buttonFind;// was used before to find anagrams on click
 	private AssetManager assetManager;
 	private TextView displayView;
 
 	@Override
-	//execution starts here
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_anagrams);
 
 		try {
-			// gets reference to the files in assets dir
 			assetManager = getAssets();
-
-			// basic object declaration
 			typeView = (EditText) findViewById(R.id.et_enter);
 
-			/*
-			"keys" file consists of all the english words in sorted order
-			the "values" file consists of all the sorted anagrams possible for a particular key.
-			both keys and values are ordered sequentially such that the line number on which
-			a particular key is present will be the same line no at which the value will be
-			present in the values file.
-			NOTE THAT CURRENTLY WE WILL ONLY BE ABLE TO FIND ANAGRAMS FOR WORDS OF MAX LENGTH OF 11
-			WE CAN ADD SUPPORT FOR MORE IF NECESSARY
-			 */
 			InputStream inputStreamKey = assetManager.open("keys");
 			InputStream inputStreamValue = assetManager.open("values");
 			assetManager = null;
 
 			/*
 			  invoke our asynchronous task for populating the hash map.
-			  the arguments from here are passed to the doInBackground() method inside it
 			 */
 			new LongOperation().execute(inputStreamKey, inputStreamValue);
 
 			/*
-			add a new listener which would check for change in the user input and fuzzy finds
-			the corresponding anagrams of the user input if exists.
+               fuzzy finder
 			 */
 			typeView.addTextChangedListener(new CustomWatcher());
 		} catch (IOException e) {
@@ -75,10 +59,6 @@ public class AnagramsActivity extends AppCompatActivity {
 		}
 	}
 
-	/*
-	Collator class is used to build searching and sorting routines for natural language text.
-	we split our string into character array. then we sort the char array and return it
-	 */
 	public String getSorted(String str) {
 		Collator collator = Collator.getInstance(new Locale("fr", "FR"));
 		String[] split = str.split("");
@@ -91,9 +71,7 @@ public class AnagramsActivity extends AppCompatActivity {
 	}
 
 	/*
-	our text listener for the the EditText typeview.
-	here we fuzzy find the anagrams on the go, without
-	relying on clicking a find button or something like that.
+       text change listener
 	 */
 	public class CustomWatcher implements TextWatcher {
 		String changed, newStr, edit, sortedInput;
@@ -101,9 +79,6 @@ public class AnagramsActivity extends AppCompatActivity {
 
 		@Override
 		public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-			/*
-			txv_display is the id given for that particular text view in the XML
-			 */
 			displayView = (TextView) findViewById(R.id.txv_display);
 			changed = s.subSequence(start, start + count).toString();// not used now. to be used in future development
 		}
@@ -115,9 +90,7 @@ public class AnagramsActivity extends AppCompatActivity {
 			 */
 			displayView.setText(null);
 			foundAtLeastOneWord = false;
-			// this the newly inputted word by the user
 			newStr = s.toString();
-			edit = s.subSequence(start, start + count).toString();// not used now. to be used in future development
 		}
 
 		/*
@@ -126,25 +99,14 @@ public class AnagramsActivity extends AppCompatActivity {
 		@Override
 		public void afterTextChanged(Editable s) {
 			boolean foundAtLeastOneWord = false;
-			/*
-			the important part where we get the alphabetically sorted form of the user input
-			so as to compare it with the values inside the keys file
-			newstr is the changed user input.
-			 */
 			sortedInput = getSorted(newStr);
 
 			/*
-			we store the key and values to a hash map named map where values are inside an ArrayList
 			eg:- iceman : [cinema,anemic]
-			get values for the key(if sortedinput is present as a key) into an array
 			 */
 			ArrayList<String> valueList = map.get(sortedInput);
 			if(valueList!=null) {
 				for (String val : valueList) {
-						/*
-						check if the sorted user input exists as a key. ie:- whether the word has anagrams
-						also make sure that the user input is not redisplayed in output
-						 */
 					if (!val.equalsIgnoreCase(newStr)) {
 						typeView.setTextColor(Color.GREEN);
 						displayView.append(val + "\n");
@@ -155,7 +117,6 @@ public class AnagramsActivity extends AppCompatActivity {
 
 			/*
 			case where no input is provided or no anagram is present for the word
-			we change text color to red as a visual cue
 			*/
 			if (!foundAtLeastOneWord)
 				typeView.setTextColor(Color.RED);
@@ -163,7 +124,7 @@ public class AnagramsActivity extends AppCompatActivity {
 	}
 
 	/*
-	perform our asynchronous task for populating the map
+	populate map with anagrams
 	 */
 	private class LongOperation extends AsyncTask<InputStream, Integer, String> {
 		@Override
@@ -171,67 +132,41 @@ public class AnagramsActivity extends AppCompatActivity {
 			super.onPreExecute();
 		}
 
-		// populate the hash map in background
 		@Override
 		protected String doInBackground(InputStream... params) {
 			map = new HashMap<>();
 			try {
-				/*
-				create buffered character stream for reading from the key and values files as characters
-				params are inputstreams of those files passed as parameters
-				 */
 				BufferedReader keyDictionary = new BufferedReader(new InputStreamReader(params[0]));
 				BufferedReader wordsDictionary = new BufferedReader(new InputStreamReader(params[1]));
 				StringTokenizer t;
 
-				// calls onProgressUpdate() method and restricts the user from inputting till map has been loaded
+				// restrict user access
 				publishProgress(1);
 
 				// start reading from both the files simultaneously since the respective key,value pairs will be on the same line
 				while (((key = keyDictionary.readLine()) != null) && (t = new StringTokenizer(wordsDictionary.readLine())) != null) {
 					anagramWords = new ArrayList<>();
 					word = "";
-					/*
-					each value read from the values file may contain more than one word. i.e:- a key may have more than
-					one anagram. so we read the values as tokens(basically splitting into different words) and store
-					to an array.
-					 */
 					while (t.hasMoreTokens()) {
 						word = t.nextToken();
 						anagramWords.add(word);
 					}
-					/*
-					we store the key and values to a map where values are inside an ArrayList
-					eg:- iceman : {cinema,anemic}
-					 */
 					map.put(key, anagramWords);
 				}
 			} catch (IOException e) {
 				System.out.println("IOError");
 			}
-			// give user control when the map is filled and ready
 			return "Type Here";
 		}
 
-		/*
-		publishProgress() would call this method which would display whats happening in background
-		disallow user from inputting till map is filled to avoid ambiguity
-		 */
 		@Override
 		protected void onProgressUpdate(Integer... values) {
 			super.onProgressUpdate(values);
 			typeView.setHintTextColor(getResources().getColor(R.color.colorPrimaryDark));
 			typeView.setHint("Loading...Please Wait");
-			/*
-			restrict user from typing inside the EditText field
-			*/
 			typeView.setFocusable(false);
 		}
 
-		/*
-		after everything is loaded give user ability to type again
-		also set a hint inside the TextField inorder make users know where to type
-		*/
 		@Override
 		protected void onPostExecute(String result) {
 			super.onPostExecute(result);
